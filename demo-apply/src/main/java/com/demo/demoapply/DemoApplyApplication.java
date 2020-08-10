a57503f5ad50ec501c0e881fb3a5d7e736bb495f
civilazation <1635107058@qq.com>
2020-08-10 11:49:52 +0800

    Initial commit

---
+++ demo-apply/src/main/java/com/demo/demoapply/DemoApplyApplication.java
@@ -0,0 +1,13 @@
+package com.demo.demoapply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplyApplication.class, args);
+    }
+
+}
